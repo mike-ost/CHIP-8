de908ed2c2a657e0935a1aa7ca898265b87c95a6
Mikael Öst <mikael.ost@gmail.com>
2021-09-02 20:26:59 +0300

    Started on display opcode

--- CHIP-8/chip8.c
+++ CHIP-8/chip8.c
@@ -5,6 +5,7 @@
 
 //#include <stdio.h>
 #include <errno.h>
+#include <SDL.h>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -19,7 +20,6 @@
 #include "inc/loadrom.h"
 
 //#define ADDRESS_ROM_START 0x200
-
 
 /* void reset_display(unsigned char *display, int size) {
 
@@ -49,16 +49,13 @@ int main()
 	unsigned char delayTimer = 0;
 	unsigned char soundTimer = 0;
 
-
 	unsigned char display[DISPLAY_HEIGHT * DISPLAY_WIDTH] = {0};
 	//unsigned char *displayP[sizeof(display)];
 
 	//keep state of keys pressed. 0 = not pressed, 1 = pressed
 	unsigned char keys[NUMBER_OF_KEYS] = {0};
 
-
-
-/* 	int e = 0;
+	/* 	int e = 0;
 	for (e = 0; e < sizeof(display); e++)
 	{
 		displayP[e] = &display[e];
@@ -71,13 +68,13 @@ int main()
 	load_rom(&memory[ADDRESS_ROM_START]);
 	init_display(display, DISPLAY_HEIGHT * DISPLAY_WIDTH);
 
-/* 	for (int x = 0; x < 80; x++)
+	/* 	for (int x = 0; x < 80; x++)
 	{
 		memory[x] = fontSet[x];
 	} */
 
 	//Read rom
-/* 	unsigned char buffer[3584] = {0};
+	/* 	unsigned char buffer[3584] = {0};
 	FILE *fptr;
 	errno_t err = fopen_s(&fptr, "C:\\temp\\roms\\ibm.ch8", "rb");
 
@@ -124,7 +121,7 @@ int main()
 
 		//unsigned short temp0 = 0;
 
-		i = opCode & 0x0FFF;
+		//i = opCode & 0x0FFF;
 		//temp0 = opCode & 0xF000;
 
 		switch (opCode & 0xF000)
@@ -132,12 +129,13 @@ int main()
 		case 0x0000:;
 			//int test1 = 1;
 
-			if (i != 0)
+			/* 			if (i != 0)
 			{
 				printf("0");
-			}
+			} */
 
-			if(i == 0x0E0) {
+			if (opCode == 0x0E0)
+			{
 				printf("reset_display");
 				reset_display(display, DISPLAY_HEIGHT * DISPLAY_WIDTH);
 			}
@@ -152,17 +150,44 @@ int main()
 			break;
 		case 0x6000:;
 			printf("6");
-			registers[(i & 0x0F00) >> 8] = i & 0x00FF;
+			registers[(opCode & 0x0F00) >> 8] = opCode & 0x00FF;
 			break;
 		case 0x7000:
 			printf("7");
-			registers[(i & 0x0F00) >> 8] += i & 0x00FF;
+			registers[(opCode & 0x0F00) >> 8] += opCode & 0x00FF;
 			break;
 		case 0xA000:
 			printf("A");
+			i = opCode & 0x0FFF;
 			break;
-		case 0xD000:
+		case 0xD000:;
 			printf("D");
+continue;
+			unsigned char x = 0;
+			unsigned char y = 0;
+			unsigned char height = 0;
+			unsigned char pixel = 0;
+			x = (opCode & 0x0F00) >> 8;
+			y = (opCode & 0x00F0) >> 4;
+			height = (opCode & 0x000F);
+
+			x = registers[x] % 64;
+			y = registers[y] % 32;
+
+			registers[0xF] = 0;
+			for(int yLine = 0; yLine < height; yLine++) {
+				pixel = memory[i+yLine];
+
+				for (int xLine = 0; xLine < 8; xLine++) {
+
+					
+				}
+
+
+
+				continue;
+			}
+
 			break;
 		default:
 			//printf("Not implemented");
